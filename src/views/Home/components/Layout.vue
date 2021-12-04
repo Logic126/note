a4bb224322e8515c3d0577c14889726c17d6b3e2
drstrong3/guofeng <guofeng@cnstrong.cn>
2021-12-04 21:17:34 +0800

    配置全局scss全局变量

---
+++ src/views/Home/components/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-layout-container">
+    <div class="layout-header-area">
+      <slot name="header" />
+    </div>
+    <div class="layout-body-area">
+      <div class="layout-sider-area">
+        <slot name="sider" />
+      </div>
+      <div class="layout-content-area">
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home-layout-container {
+  background-color: #fff;
+  .layout-header-area {
+    border-bottom: 1px solid $primary-color;
+  }
+  .layout-body-area {
+  }
+}
+</style>
